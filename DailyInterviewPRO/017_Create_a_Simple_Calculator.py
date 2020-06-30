32d5acdc364bc8a3ebbb25924e9054b017333ce0
vsanelli <valerio.sanelli@gmail.com>
2020-06-30 22:35:11 +0200

    Updated DailyInterviewPRO problems

---
+++ DailyInterviewPRO/017_Create_a_Simple_Calculator.py
@@ -0,0 +1,12 @@
+""" Given a mathematical expression with just single digits, plus signs, negative signs, and brackets, 
+evaluate the expression. Assume the expression is properly formed.
+
+Example:
+Input: - ( 3 + ( 2 - 1 ) )
+Output: -4
+"""
+def eval(expression):
+  # Fill this in.
+
+print eval('- (3 + ( 2 - 1 ) )')
+# -4
