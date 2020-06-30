654a7fa9fd0e77e07f6f5453324ac74704898335
vsanelli <valerio.sanelli@gmail.com>
2020-07-01 00:14:35 +0200

    added solution to 009_Find_the_non_duplicate_number.py

--- DailyInterviewPRO/009_Find_the_non_duplicate_number.py
+++ DailyInterviewPRO/009_Find_the_non_duplicate_number.py
@@ -7,8 +7,17 @@ Output: 1
 
 Challenge: Find a way to do this using O(1) memory.
 """
-def singleNumber(nums):
-  # Fill this in.
 
-print singleNumber([4, 3, 2, 4, 1, 3, 2])
+
+def singleNumber(nums):
+    nums.sort()
+    res = None
+    for index, x in enumerate(nums[:-1:2]):
+        if nums[index + 1] != x:
+            res = x
+    return res
+
+
+print(singleNumber([4, 3, 2, 4, 1, 3, 2]))
+# print(singleNumber([4, 3, 2, 4, 1, 3, 2]))
 # 1
